6cb9d15f5d72434a5100fc242bf2edd798beba55
Guillermo Sanchez <guillesafran@gmail.com>
2023-02-21 14:28:55 -0300

    Add error message if the input-form is submitted empty

--- src/components/ShortenLinkForm.jsx
+++ src/components/ShortenLinkForm.jsx
@@ -1,10 +1,11 @@
-import { useContext, useRef } from 'react'
+import { useContext, useRef, useState } from 'react'
 import { userURLContext } from '../context/URLsContext'
 import getShortenLink from '../services/getShortenLink'
 
 function ShortenLinkForm () {
   const { setUserURL } = useContext(userURLContext)
   const linkToShort = useRef('')
+  const [userInputURLBlank, setUserInputURLBlank] = useState(false)
 
   function verifyUrl (e) {
     linkToShort.current = e.target.value
@@ -12,7 +13,8 @@ function ShortenLinkForm () {
 
   async function onSubmitLink (e) {
     e.preventDefault()
-    if (linkToShort?.current.length === 0) return
+    if (linkToShort?.current.length === 0) return setUserInputURLBlank(() => true)
+    setUserInputURLBlank(() => false)
     const validURL = linkToShort.current.match(/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g) //eslint-disable-line
     if (validURL === null) return
     const shorthenLink = await getShortenLink(linkToShort.current)
@@ -25,6 +27,7 @@ function ShortenLinkForm () {
         <form onSubmit={onSubmitLink}>
           <div className='bg-form' />
           <input onChange={verifyUrl} type='text' placeholder='Shorten a link here...' />
+          {userInputURLBlank && <p>Please add a link</p>}
           <button type='submit'>Shorten It!</button>
         </form>
       </div>
